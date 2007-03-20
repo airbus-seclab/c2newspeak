ae051bf87210e8fa7ce130df778579872a504655
yeye(at)localhost.localdomain <devnull@localhost>
2007-03-20 11:45:08 +0100

    Some changes in npkstats so as it compiles, but does nothing

--- src/npkstats/main.ml
+++ src/npkstats/main.ml
@@ -1,38 +1,38 @@
-let fnames = ref []
-  
-let usage_msg = ""
+let fname = ref ""
 
-let anon_fun x = fnames := x::!fnames
+let anon_fun file =
+  if !fname = ""
+  then fname := file
+  else invalid_arg "You can only get statistics on one file at a time."
+
+let usage_msg = Sys.argv.(0)^" [options] [-help|--help] file.npk"
 
 let speclist = 
-  (* Newspeak options *)
-  Npkcontext.argslist
-  (* Verbose options *)
-  @Npkcontext.verb_argslist
-  @Npkstats.args
-  @[("-v", Arg.Unit (Npkcontext.verbose true),
+  Npkstats.args
+(*  @[("-v", Arg.Unit (Npkcontext.verbose true),
      "verbose mode: turn all verbose options on");
     
     ("-q", Arg.Unit (Npkcontext.verbose false), 
      "quiet mode: turn display off");
     
     ("--exit-code", Arg.Set Npkcontext.exit_code, "");		  
-    ("-c", Arg.Set Npkcontext.exit_code, "returns 1 if an error occured")]
+    ("-c", Arg.Set Npkcontext.exit_code, "returns 1 if an error occured")] *)
     
 let _ = 
   Arg.parse speclist anon_fun usage_msg;
   
-  if !fnames = [] then begin
+  if !fname = "" then begin
     print_endline ("Fatal error: no file specified. Try "
 		   ^Sys.argv.(0)^" --help");
-    exit (if !Npkcontext.exit_code then 1 else 0);
+    exit 0;
   end;
   
   try
-    let prog = Cil2newspeak.cil2newspeak !fnames in
+    let prog = (*Cil2newspeak.cil2newspeak !fnames in*)
+      ([], [], Hashtbl.create 100) in
       Collector.count prog;
       print_endline (Npkstats.to_string ())
   with Invalid_argument s -> 
     print_endline ("Fatal error: "^s);
-    exit (if !Npkcontext.exit_code then 1 else 0)
+    exit 0
       
