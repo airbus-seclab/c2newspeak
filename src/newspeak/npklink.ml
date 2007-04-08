2b2a629b84e349b6c0ba50b57081e40a9b39d047
Charles Hymans <charles.hymans(at)gmail.com>
2007-04-08 11:31:42 +0200

    Cleaning up: glb_used is now local to link phase. This is one step towards the splitting of npkenv for compile and npkenv for linking.

--- src/newspeak/npklink.ml
+++ src/newspeak/npklink.ml
@@ -1,51 +1,42 @@
 open Cil
 open Npkutils
 open Npkcontext
-open Npkenv
 open Npkil
 
-
 let filenames = ref []
+
+let glb_used = ref (String_set.empty)
 
 let handle_file npko =
   (* TODO: Print debug... Handling file... *)
   filenames := npko.ifilename::(!filenames);
   glb_used := String_set.union !glb_used npko.iusedglbs;
-  fun_called := String_set.union !fun_called npko.iusedfuns;
-  glb_cstr := String_set.union !glb_cstr npko.iusedcstr;
-  Hashtbl.iter update_glob_link npko.iglobs;
-  Hashtbl.iter update_fun_link npko.ifuns
+  Npkenv.fun_called := String_set.union !Npkenv.fun_called npko.iusedfuns;
+  Npkenv.glb_cstr := String_set.union !Npkenv.glb_cstr npko.iusedcstr;
+  Hashtbl.iter Npkenv.update_glob_link npko.iglobs;
+  Hashtbl.iter Npkenv.update_fun_link npko.ifuns
 
-
-
-
-
-
-let generate_globals globs g_used cstrs =
-  String_set.iter handle_cstr cstrs;
+let generate_globals globs cstrs =
+  String_set.iter Npkenv.handle_cstr cstrs;
 (* TODO: translate_exp should not be exported ? *)
-  Hashtbl.iter (handle_real_glob Npkcompile.translate_exp g_used) globs;
-  get_glob_decls ()  
+  Hashtbl.iter (Npkenv.handle_real_glob Npkcompile.translate_exp !glb_used) 
+    globs;
+  Npkenv.get_glob_decls ()
 
-
-
-	
 let generate_funspecs funs f_called =
-  Hashtbl.iter (handle_funspec f_called) funs;
-  get_funspecs ()
-
-
+  Hashtbl.iter (Npkenv.handle_funspec f_called) funs;
+  Npkenv.get_funspecs ()
 
 let link npkos =
   (* TODO: Think about it *)
   update_loc Cil.locUnknown;
 
-  init_env ();
+  Npkenv.init_env ();
 
   print_debug "Linking files...";
   List.iter handle_file npkos;
-  let decls = generate_globals glb_decls !glb_used !glb_cstr in
-  let funs = generate_funspecs fun_specs !fun_called in
+  let decls = generate_globals Npkenv.glb_decls !Npkenv.glb_cstr in
+  let funs = generate_funspecs Npkenv.fun_specs !Npkenv.fun_called in
   let kernel = ([], decls, funs) in
   print_debug "File linked.";
 
