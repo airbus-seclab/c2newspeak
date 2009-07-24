11ddcfbd22a6551bf64234e5ff9a6418302a365c
Etienne Millon <etienne . millon AT gmail . com>
2009-07-24 14:23:59 +0200

    added type coercion for unary minus

---
+++ tests/ada2newspeak/t317.adb
@@ -0,0 +1,10 @@
+-- Unary minus and type coercion.
+--
+-- Author : Etienne Millon
+-- Date   : Thu Jul 23 2009
+--
+procedure t317 is
+  X : Integer;
+begin
+    X := -1;
+end t317 ;
---
+++ tests/ada2newspeak/t317.spec
@@ -0,0 +1,9 @@
+Newspeak output
+---------------
+t317.adb
+void t317(void) {
+  (t317.adb:7#5)^int32 x;
+  (t317.adb:9#8)^0- =(int32) -1;
+}
+
+
