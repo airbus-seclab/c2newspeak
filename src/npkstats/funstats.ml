f787d2810934a4c2d4ebe3cbd449e7d02143ce06
Charles Hymans <charles.hymans@eads.net>
2009-07-31 15:11:48 +0200

    Added category of functions which deref locals.

--- src/npkstats/funstats.ml
+++ src/npkstats/funstats.ml
@@ -25,18 +25,64 @@
 
 open Newspeak
 
-module Set = Set.Make(String)
+module GlbSet = Set.Make(String)
+module LocSet = Set.Make(struct type t = int let compare = compare end)
+
+type env =  {
+  height: int;
+  glb_read: GlbSet.t;
+  glb_write: GlbSet.t;
+  loc_read: LocSet.t;
+  loc_write: LocSet.t
+}
+
+let init_env () = {
+  height = 0;
+  glb_read = GlbSet.empty;
+  glb_write = GlbSet.empty;
+  loc_read = LocSet.empty;
+  loc_write = LocSet.empty
+}
+
+let write_env (g, l) env = 
+  { 
+    env with 
+      glb_write = GlbSet.union g env.glb_write;
+      loc_write = LocSet.union l env.loc_write 
+  }
+let read_env (g, l) env = 
+  { 
+    env with 
+      glb_read = GlbSet.union g env.glb_read; 
+      loc_read = LocSet.union l env.loc_read
+  }
+
+let join_env env1 env2 = {
+  env1 with
+    glb_read = GlbSet.union env1.glb_read env2.glb_read;
+    glb_write = GlbSet.union env1.glb_write env2.glb_write;
+    loc_read = LocSet.union env1.loc_read env2.loc_read;
+    loc_write = LocSet.union env1.loc_write env2.loc_write
+}
+
+let global_is_used env = 
+  not (GlbSet.is_empty env.glb_read) || not (GlbSet.is_empty env.glb_write)
+
+let local_is_deref env =
+  not (LocSet.is_empty env.loc_read) || not (LocSet.is_empty env.loc_write)
 
 type funkind = 
     Empty
   | Pure
-  | Globals of (Set.t * Set.t)
+  | Globals of env
+  | LocDeref of env
   | Other
 
 let print_results funtbl =
   let empty_nb = ref 0 in
   let pure_nb = ref 0 in
   let global_nb = ref 0 in
+  let deref_nb = ref 0 in
   let other_nb = ref 0 in
   let other = ref [] in
   let count f kind =
@@ -45,6 +91,7 @@ let print_results funtbl =
 	  Empty -> empty_nb
 	| Pure -> pure_nb
 	| Globals _ -> global_nb
+	| LocDeref _ -> deref_nb
 	| Other -> 
 	    other := f::!other;
 	    other_nb
@@ -60,6 +107,9 @@ let print_results funtbl =
 	print_endline ("Functions that read/update globals: "
 		       ^string_of_int !global_nb)
       end;
+      if !deref_nb <> 0 then begin
+	print_endline ("Functions that deref locals: "^string_of_int !deref_nb)
+      end;
       if !other_nb <> 0 then begin
   	print_endline "Remaining functions: ";
 	List.iter print_endline !other
@@ -69,96 +119,79 @@ let print_results funtbl =
 let collect prog = 
   let funtbl = Hashtbl.create 100 in
 
-  let written_globals = ref Set.empty in
-  let read_globals = ref Set.empty in
-
-  let rec write_lval lv =
+  let rec process_lval env lv =
     match lv with
 	(* Assumes local do not *)
-	Local _ -> ()
-      | Global x -> written_globals := Set.add x !written_globals
+	Local _ -> (GlbSet.empty, LocSet.empty)
+      | Global x -> (GlbSet.singleton x, LocSet.empty)
+      | Deref (Lval (Local x, _), _) -> 
+	  (GlbSet.empty, LocSet.singleton (env.height - x))
       | _ -> raise Exit
 
-  and write_exp e =
+  and process_exp env e =
     match e with
-	Const _ -> ()
-      | Lval (lv, _) -> write_lval lv
-      | UnOp (Not, e) -> write_exp e
+	Const _ -> (GlbSet.empty, LocSet.empty)
+      | Lval (lv, _) -> process_lval env lv
+      | UnOp ((PtrToInt _|IntToPtr _|Cast _|Not), e) -> process_exp env e
       | BinOp ((Eq _|Gt _), e1, e2) -> 
-	  write_exp e1;
-	  write_exp e2
-      | _ -> raise Exit
-  in
-    
-  let rec read_lval lv =
-    match lv with
-	Local _ -> ()
-      | _ -> raise Exit
-
-  and read_exp e =
-    match e with
-	Const _ -> ()
-      | Lval (lv, _) -> read_lval lv
-      | UnOp ((PtrToInt _|IntToPtr _|Cast _), e) -> read_exp e
+	  let (glb1, loc1) = process_exp env e1 in
+	  let (glb2, loc2) = process_exp env e2 in
+	    (GlbSet.union glb1 glb2, LocSet.union loc1 loc2)
       | _ -> raise Exit
   in
 
-  let rec process_stmtkind x =
+  let rec process_stmtkind env x =
     match x with
 	Set (lv, e, _) -> 
-	  write_lval lv; 
-	  read_exp e
-      | Decl (_, _, body) -> process_blk body
+	  let env = write_env (process_lval env lv) env in
+	    read_env (process_exp env e) env
+      | Decl (_, _, body) -> 
+	  let env = { env with height = env.height + 1 } in
+	  let env = process_blk env body in
+	    { env with height = env.height - 1 }
       | Select (br1, br2) -> 
-	  process_blk br1;
-	  process_blk br2
-      | Guard e -> write_exp e
+	  let env = process_blk env br1 in
+	    process_blk env br2
+      | Guard e -> write_env (process_exp env e) env
       | DoWith (body, _, action) -> 
-	  process_blk body;
-	  process_blk action
-      | Goto _ -> ()
+	  let env = process_blk env body in
+	    process_blk env action
       | Call FunId f -> begin
 	  match process_fun f with
-	      Empty | Pure -> ()
-	    | Globals (read, written) -> 
-		written_globals := Set.union written !written_globals;
-		read_globals := Set.union read !read_globals
+	      Empty | Pure -> env
+	    | Globals env_f -> join_env env env_f
+	    | LocDeref _ -> raise Exit
 	    | Other -> raise Exit
 	end
-      | InfLoop body -> process_blk body
+      | InfLoop body -> process_blk env body
+      | Goto _ -> env
       | _ -> raise Exit
     
-  and process_blk x = 
+  and process_blk env x = 
     match x with
 	(hd, _)::tl -> 
-	  process_stmtkind hd;
-	  process_blk tl
-      | [] -> ()
+	  let env = process_stmtkind env hd in
+	    process_blk env tl
+      | [] -> env
 
   and process_fun f =
     try Hashtbl.find funtbl f 
     with Not_found -> 
       try
 	let (_, blk) = Hashtbl.find prog.fundecs f in
+	let env = init_env () in
 	let kind =
 	  match blk with
 	      [] -> Empty
 	    | _ -> 
-		let written = !written_globals in
-		let read = !read_globals in 
-		  written_globals := Set.empty;
-		  read_globals := Set.empty;
-		  let kind = 
-		    try 
-		      process_blk blk;
-		      if (Set.is_empty !written_globals)
-			&& (Set.is_empty !read_globals) then Pure
-		      else Globals (!read_globals, !written_globals)
-		    with Exit -> Other
-		  in
-		    written_globals := written;
-		    read_globals := read;
-		    kind
+		try 
+		  let env = process_blk env blk in
+		  let g = global_is_used env in
+		  let l = local_is_deref env in
+		    if not g && not l then Pure
+		    else if not l then Globals env
+		    else LocDeref env
+		with Exit -> Other
 	in
 	  Hashtbl.add funtbl f kind;
 	  kind
