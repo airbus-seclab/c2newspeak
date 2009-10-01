45c0016e0b897be4081329f8da04b901c7b9c177
Charles Hymans <charles.hymans@gmail.com>
2009-10-01 21:39:31 +0900

    npknull: added non-regression tests todo.

---
+++ tests/npknull/todo/precision/003.c
@@ -0,0 +1,40 @@
+/*
+  C2Newspeak: compiles C code into Newspeak. Newspeak is a minimal language 
+  well-suited for static analysis.
+  Copyright (C) 2007  Charles Hymans, Olivier Levillain
+  
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+  
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+  
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+  Charles Hymans
+  EADS Innovation Works - SE/CS
+  12, rue Pasteur - BP 76 - 92152 Suresnes Cedex - France
+  email: charles.hymans@penjili.org
+*/
+
+char t[1];
+char u[1];
+
+void f(char *) {
+}
+
+void main() {
+  char * options[] = { &t[0], &u[0] };
+  int *ptr;
+  int x;
+
+  ptr = &x;
+  f(options[0]);
+  *ptr = 1; // precision: should not display any null pointer deref here
+}
