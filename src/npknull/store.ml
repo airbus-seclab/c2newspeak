0055a6e8e4204b856a2adc631c7c6ae290587357
Charles Hymans <charles.hymans@gmail.com>
2009-10-05 23:24:11 +0900

    npknull: removed another case of forget.

--- src/npknull/store.ml
+++ src/npknull/store.ml
@@ -229,7 +229,7 @@ let translate_exp_P1 env s e =
 	    [P1.AddrOf (m, Some (o, n))]
       | UnOp ((Coerce _|Cast (Int _, FunPtr)|PtrToInt _), e)
       | BinOp ((PlusPI|DivI|Mod), e, _) -> translate e
-      | BinOp ((PlusI|MinusI|MultI|Shiftrt|BAnd _|BOr _), e1, e2) ->
+      | BinOp ((PlusI|MinusI|MultI|Shiftrt|BAnd _|BOr _|MinusPP), e1, e2) ->
 	  let v1 = translate e1 in
 	  let v2 = translate e2 in
 	    v1@v2
@@ -250,7 +250,7 @@ let translate_exp_P3 env s e =
       | AddrOfFun (f, _) -> [P3.AddrOfFun f]
       | UnOp ((Coerce _|Cast _|PtrToInt _), e) 
       | BinOp ((PlusPI|DivI|Mod), e, _) -> translate e
-      | BinOp ((PlusI|MinusI|MultI|Shiftrt|BAnd _|BOr _), e1, e2) ->
+      | BinOp ((PlusI|MinusI|MultI|Shiftrt|BAnd _|BOr _|MinusPP), e1, e2) ->
 	  (translate e1)@(translate e2)
       | _ -> 
 	  print_endline "Store.translate_exp_P3";
---
+++ tests/npknull/104.spec
@@ -0,0 +1,1 @@
+104.c:35#2: potential null pointer deref
