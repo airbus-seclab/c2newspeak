2644c5dc6faf174eeb2ecc9d11d919c0dff72537
Etienne Millon <etienne . millon AT gmail . com>
2009-10-13 15:15:19 +0200

    + 'prove'

--- solver.ml
+++ solver.ml
@@ -1,19 +1,25 @@
 
 let display_help _ =
-  prerr_endline ("Usage : "^Sys.argv.(0)^" file.npk")
+  print_endline ("Usage : "^Sys.argv.(0)^" file.npk")
 
 let display_version _ =
-  prerr_endline "V. pre-alpha"
+  print_endline "Version : pre-alpha";
+  print_endline "License : LGPLv2";
+  print_endline "Author  : Etienne Millon";
+  print_endline "Contact : etienne DOT millon AT eads DOT net"
 
 let handle_file fname =
   let npk = Newspeak.read fname in
-  prerr_endline (String.concat "," npk.Newspeak.fnames)
+  print_endline (String.concat "," npk.Newspeak.fnames)
 
 let main _ =
   let optlist = [ 'h', "help"    , Some display_help,    None
                 ; 'v', "version" , Some display_version, None
                 ] in
-  Getopt.parse_cmdline optlist handle_file
+  if (Array.length Sys.argv = 1) then
+    display_help ()
+  else
+    Getopt.parse_cmdline optlist handle_file
 
 let _ =
   main ()
---
+++ t/01usage.t
@@ -0,0 +1,20 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use Test::Command tests => 12;
+
+my $cmd = './solver';
+
+exit_is_num ($cmd, 0, 'Silent fail is not an error');
+stdout_like ($cmd, qr/^Usage/, 'Help message without args');
+
+foreach (qw/-h --help/) {
+  stdout_like ([$cmd,$_], qr/^Usage/, 'Help message with '.$_);
+}
+
+foreach (qw/-v --version/) {
+  stdout_like ([$cmd,$_], qr/^Version/,                           'Version with '.$_);
+  stdout_like ([$cmd,$_], qr/LGPLv2/,                             'License with '.$_);
+  stdout_like ([$cmd,$_], qr/Etienne Millon/,                     'Author with ' .$_);
+  stdout_like ([$cmd,$_], qr/etienne DOT millon AT eads DOT net/, 'Email with '  .$_);
+}
