a70b7436f315209b5233739af9dddca556177bf8
Charles Hymans <charles.hymans@eads.net>
2009-10-28 14:46:10 +0100

    benchmarks: cleaned up some tests, added a new one.

--- tests/benchmarks/019.c
+++ tests/benchmarks/019.c
@@ -1,9 +1,30 @@
-void my_memcpy(char *dst, char *src, int n) {
-  int i;
-  for (i = 0; i < n; i++) {
-    dst[i] = src[i];
-  }
-}
+/*
+  C2Newspeak: compiles C code into Newspeak. Newspeak is a minimal language 
+  well-suited for static analysis.
+  Copyright (C) 2007  Charles Hymans, Olivier Levillain
+  
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+  
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+  
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+  Charles Hymans
+  EADS Innovation Works - SE/CS
+  12, rue Pasteur - BP 76 - 92152 Suresnes Cedex - France
+  email: charles.hymans@penjili.org
+*/
+
+// pointers in structures
+// interest: low
 
 typedef struct {
   int a;
@@ -11,17 +32,14 @@ typedef struct {
 } S;
 
 int witness;
-int x;
-S s1 = {0, &witness};
 
 int main() {
-  S s2;
+  S s;
   char t[10];
   witness = 0;
-  my_memcpy(&s2, &s1, sizeof(S));
-  s2.ptr = &witness;
-  *s2.ptr = 10000000;
-  // should signal an error since s2.ptr points to witness that is 
+  s.ptr = &witness;
+  *s.ptr = 10000000;
+  // should signal an error since s.ptr points to witness that is 
   // then set to 1000000
   t[witness] = 0; 
   return 0;
--- tests/benchmarks/020.c
+++ tests/benchmarks/020.c
@@ -1,3 +1,27 @@
+/*
+  C2Newspeak: compiles C code into Newspeak. Newspeak is a minimal language 
+  well-suited for static analysis.
+  Copyright (C) 2007  Charles Hymans, Olivier Levillain
+  
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+  
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+  
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+  Charles Hymans
+  EADS Innovation Works - SE/CS
+  12, rue Pasteur - BP 76 - 92152 Suresnes Cedex - France
+  email: charles.hymans@penjili.org
+*/
 
 int x;
 
@@ -13,48 +37,9 @@ int main() {
   
   onecpy((char*)&p1, (char*)&p2);
   
-  // should be a warning here
-  *p2 = 1;
+  // soundness: should be a warning here (since only one byte is copied)
+  // similar to 006.c
+  *p1 = 1;
   
   return 0;
 }
-
-
-/* Result of airac:
-*************       ./airac5 000.c      ******************
-
-
-Airac5: public-1.0, analyzing 000.c
-= Progress =
-Parsing. (0.00s)
-Transforming..... (0.00s)
-Computing fixpoint with widening. (0.00s)
-Computing fixpoint with narrowing (0.00s)
-Inspecting result. (0.00s)
-Refining alarms (0.00s)
-
-= Input Program Size =
-Procedures: 3
-Blocks:     5
-Commands:   16
-
-= Analysis Parameters =
-Analyze every procedure: no
-Inlining depth: 1
-Unrolling bound: 0
-Expected iterations: 300
-
-= Analysis Times =
-Total: 0.00s
-Parsing: 0.00s
-Transforming: 0.00s
-Computing fixpoint with widening: 0.00s
-Computing fixpoint with narrowing: 0.00s
-Inspecting result: 0.00s
-Refining alarms: 0.00s
-
-= Alarms =
-Total number of alarms: 0
------------------------------------------------
-Airac misses the warning, it is unsound
- */
