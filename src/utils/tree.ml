cc5a6303d2179fd010538db7d32e63ab32551d2a
Etienne Millon <etienne . millon AT gmail . com>
2010-06-29 17:12:39 +0200

    Ada & utils : removed "first child/next sibling" tree (unused).

--- src/utils/tree.ml
+++ src/utils/tree.ml
@@ -1,166 +1,54 @@
-module type TREE = sig
-  type 'a t
-  val create : unit -> 'a t
-  val push   : 'a -> 'a t -> unit
-  val pop    : 'a t -> 'a
-  val top    : 'a t -> 'a
-  val lookup : ('a -> 'b option) -> 'a t -> 'b option
-  val iter   : ('a -> unit) -> 'a t -> unit
-  val fold   : ('res -> 'a -> 'res) -> 'res -> 'a t -> 'res
-  val height : 'a t -> int
-  val nth    : 'a t -> int -> 'a
+(*
+  C2Newspeak: compiles C code into Newspeak. Newspeak is a minimal language
+  well-suited for static analysis.
+  Copyright (C) 2007  Charles Hymans, Olivier Levillain
 
-  val first_child  : 'a t -> unit
-  val next_sibling : 'a t -> unit
-end
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
 
-module StackedTree : TREE = struct
-  type 'a t = 'a Stack.t
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
 
-  let create = Stack.create
-  let push   = Stack.push
-  let top    = Stack.top
-  let pop    = Stack.pop
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*)
 
-  let lookup p s =
-    let s = Stack.copy s in
-    let r = ref None in
-    while (!r = None && not (Stack.is_empty s)) do
-      let t = Stack.pop s in
-        r := p t
-    done;
-    !r
+(** @author Etienne Millon <etienne.millon@eads.net> *)
 
-  let iter = Stack.iter
+type 'a t = 'a Stack.t
 
-  let height = Stack.length
+let create = Stack.create
+let push   = Stack.push
+let top    = Stack.top
+let pop    = Stack.pop
 
-  let nth s n =
-    let s = Stack.copy s in
-    while (height s > n) do
-      pop s;
-    done;
-    top s
+let lookup p s =
+  let s = Stack.copy s in
+  let r = ref None in
+  while (!r = None && not (Stack.is_empty s)) do
+    let t = Stack.pop s in
+      r := p t
+  done;
+  !r
 
-  let fold f init s =
-    let r = ref init in
-    iter (fun x -> r := f !r x) s;
-    !r
+let iter = Stack.iter
 
-  let first_child _ =
-    failwith "FC/NS in StackedTree"
+let height = Stack.length
 
-  let next_sibling  _ =
-    failwith "FC/NS in StackedTree"
+let nth s n =
+  let s = Stack.copy s in
+  while (height s > n) do
+    pop s;
+  done;
+  top s
 
-end
-
-
-module FCNSTree : TREE = struct
-
-  type 'a t = 'a tree ref
-
-  and 'a tree = 'a node option
-
-  and 'a node = {         fcns_contents    : 'a
-                ;         fcns_parent      : 'a tree
-                ; mutable fcns_firstchild  : 'a tree
-                ; mutable fcns_nextsibling : 'a tree
-                }
-
-  let create _ =
-    ref None
- 
-  let iter f t =
-    let rec iter_tree f t =
-      match t with
-      | None   -> ()
-      | Some n -> begin
-                    f n.fcns_contents;
-                    iter_tree f n.fcns_parent
-                  end
-    in
-    iter_tree f !t
-  
-  let fold f init s =
-    let r = ref init in
-    iter (fun x -> r := f !r x) s;
-    !r
-
-  let top x =
-    match !x with
-    | None -> invalid_arg "FCNSTree.top"
-    | Some n -> n.fcns_contents
-
-  (* Path from root to x *)
-  let path (x:'a t) =
-    fold (fun r n -> n::r) [] x
-
-  let height x =
-    fold (fun r _ -> succ r) 0 x
-
-  let nth x n =
-    List.nth (path x) (n-1)
-
-  let lookup p x =
-    let rec lookup_tree p x =
-      match x with
-       | None  -> None
-       | Some n -> begin
-                     match p n.fcns_contents with
-                     | Some x -> Some x
-                     | None   -> lookup_tree p n.fcns_parent
-                   end
-    in
-    lookup_tree p !x
-
-  let pop x =
-    match !x with
-    | None   -> invalid_arg "FCNSTree.pop"
-    | Some n -> begin
-                  let return_me = top x in
-                  x := n.fcns_parent;
-                  return_me
-                end
-
-  let push (e:'a) (x:'a t) :unit =
-    let newnode = { fcns_contents    = e
-                  ; fcns_parent      = !x
-                  ; fcns_firstchild  = None
-                  ; fcns_nextsibling = None
-                  }
-    in
-    (* Compute NS(NS(NS(...(x)))) -- 0 times or more until the node has no NS *)
-    let rec nsstar (x:'a node) :'a node =
-      match x.fcns_nextsibling with
-      | None   -> x
-      | Some s -> nsstar s
-    in
-    let push_node (x:'a node) =
-      begin
-        match x.fcns_firstchild with
-        | None    -> x.fcns_firstchild             <- Some newnode
-        | Some fc -> (nsstar(fc)).fcns_nextsibling <- Some newnode
-      end
-    in
-    begin
-      match !x with
-      | None      -> ()
-      | Some node -> push_node node
-    end;
-    x := Some newnode
-
-    let first_child x =
-      match !x with
-      | None   -> failwith "first_child"
-      | Some n -> x:= n.fcns_firstchild
-
-    let next_sibling x =
-      match !x with
-      | None   -> failwith "next_sibling"
-      | Some n -> x:= n.fcns_nextsibling
-
-end
-
-
+let fold f init s =
+  let r = ref init in
+  iter (fun x -> r := f !r x) s;
+  !r
 
--- src/utils/tree.mli
+++ src/utils/tree.mli
@@ -1,20 +1,32 @@
+(*
+  C2Newspeak: compiles C code into Newspeak. Newspeak is a minimal language
+  well-suited for static analysis.
+  Copyright (C) 2007  Charles Hymans, Olivier Levillain
 
-module type TREE = sig
-  type 'a t
-  val create : unit -> 'a t
-  val push   : 'a -> 'a t -> unit
-  val pop    : 'a t -> 'a
-  val top    : 'a t -> 'a
-  val lookup : ('a -> 'b option) -> 'a t -> 'b option
-  val iter   : ('a -> unit) -> 'a t -> unit
-  val fold   : ('res -> 'a -> 'res) -> 'res -> 'a t -> 'res
-  val height : 'a t -> int
-  val nth    : 'a t -> int -> 'a
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
 
-  val first_child  : 'a t -> unit
-  val next_sibling : 'a t -> unit
-end
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
 
-module StackedTree : TREE
-module FCNSTree    : TREE
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*)
 
+(** @author Etienne Millon <etienne.millon@eads.net> *)
+
+type 'a t
+val create : unit -> 'a t
+val push   : 'a -> 'a t -> unit
+val pop    : 'a t -> 'a
+val top    : 'a t -> 'a
+val lookup : ('a -> 'b option) -> 'a t -> 'b option
+val iter   : ('a -> unit) -> 'a t -> unit
+val fold   : ('res -> 'a -> 'res) -> 'res -> 'a t -> 'res
+val height : 'a t -> int
+val nth    : 'a t -> int -> 'a
