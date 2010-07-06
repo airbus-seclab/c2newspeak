b0bc35e6f3a6dbebe8eb1adf3d3ffc1be826b150
Etienne Millon <etienne . millon AT gmail . com>
2010-07-06 11:07:08 +0200

    npksolver : added copyright information

--- src/npksolver/pair.ml
+++ src/npksolver/pair.ml
@@ -15,6 +15,9 @@
   You should have received a copy of the GNU Lesser General Public
   License along with this library; if not, write to the Free Software
   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+  Copyright 2009, 2010 Etienne Millon <etienne.millon@eads.net>
+
  *)
 
 (** @author Etienne Millon <etienne.millon@eads.net> *)
