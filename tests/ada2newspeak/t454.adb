738aca0193268ffc3391e85e114dc0c589550d01
Wenceslas Godard <wenceslas.godard(at)eads.net>
2011-01-10 17:53:01 +0100

    fixing bug for inclusion of packges from spec + adding test

---
+++ tests/ada2newspeak/t454.adb
@@ -0,0 +1,7 @@
+package body T454 is
+   procedure Gg is
+      X : T454a.Integ;
+   begin
+      null;
+   end Gg;
+end T454;
---
+++ tests/ada2newspeak/t454.spec
@@ -0,0 +1,7 @@
+Newspeak output
+---------------
+void t454.gg(void) {
+  (t454.adb:3#9)^int32 x;
+}
+
+
