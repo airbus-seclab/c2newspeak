a462e74708acc1cb0464943d28f8c09a1bf2aad7
Etienne Millon <etienne . millon AT eads . net>
2012-02-01 16:13:36 +0100

    ptrtype: propagate type checks on control flow

--- src/ptrtype/simple.ml
+++ src/ptrtype/simple.ml
@@ -21,6 +21,83 @@
  * 12, rue Pasteur - BP 76 - 92152 Suresnes Cedex - France
  *)
 
-type simple = unit
+module T = Tyspeak
+module N = Newspeak
 
-let check _ = ()
+let hashtbl_values
+  : (('a, 'b) Hashtbl.t -> 'b list)
+  = fun h ->
+    Hashtbl.fold (fun _k v l -> v::l) h []
+
+type simple =
+  | Int
+  | Ptr of simple
+
+let scalar_compatible st ty =
+  match (st, ty) with
+  | (N.Int _, Int) -> true
+  | (N.Ptr, Ptr _) -> true
+  | _ -> false
+
+let tc_assert ok =
+  if not ok then
+    assert false
+
+let same_type (x:simple) y =
+  tc_assert (x = y)
+
+let check_exp (_be, _te) =
+  (* check subexps *)
+  (* check result *)
+  ()
+
+let from_env env k =
+  List.assoc k env
+
+let rec check_stmt env (sk, _loc) =
+  match sk with
+  | T.Set (lv, e, st) ->
+      let (_be, te) = e in
+      let t_lv = from_env env lv in
+      check_exp e;
+      tc_assert (scalar_compatible st te);
+      same_type te t_lv;
+      ()
+  | T.Copy (dst, src, _sz) ->
+      let tdst = from_env env dst in
+      let tsrc = from_env env src in
+      same_type tdst tsrc
+  | T.Guard e ->
+      let (_be, te) = e in
+      check_exp e;
+      same_type te Int
+  | T.Decl (vid, ty, blk) ->
+      let new_env = (T.Local vid, ty)::env in
+      check_blk new_env blk
+  | T.Select (a, b) ->
+      check_blk env a;
+      check_blk env b
+  | T.InfLoop blk ->
+      check_blk env blk
+  | T.DoWith (blk, _lbl) ->
+      (* TODO add lbl in environment *)
+      check_blk env blk
+  | T.Goto _lbl ->
+      (* TODO check if lbl is in environment *)
+      ()
+  | T.Call (_args, _fe, _ret) ->
+      assert false
+  | T.UserSpec _ -> ()
+
+and check_blk env =
+  List.iter (check_stmt env)
+
+let check tpk =
+  let init_env = [] in
+  let fun_blocks =
+    List.map
+      (fun fdec -> fdec.T.body)
+      (hashtbl_values tpk.T.fundecs)
+  in
+  let all_blocks = tpk.T.init::fun_blocks in
+  List.iter (check_blk init_env) all_blocks
